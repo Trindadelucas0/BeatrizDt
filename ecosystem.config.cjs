dc0b9d23f6f0afaaf2aecf6eadd7bbf02388ed69
Lucas Rodrigues <Lucasrodrigues4@live.com>
2026-07-01 10:29:19 -0300

    s

---
+++ ecosystem.config.cjs
@@ -0,0 +1,18 @@
+const path = require('node:path');
+
+module.exports = {
+  apps: [
+    {
+      name: 'beatriz-dt',
+      cwd: path.join(__dirname),
+      script: 'server.js',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3454,
+      },
+      max_restarts: 10,
+      restart_delay: 3000,
+      autorestart: true,
+    },
+  ],
+};
